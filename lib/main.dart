994ad3d0c14d2cd70e574088ca26646ad42a05ec
Nirob-0812 <mehedihasannirobcsediu@gmail.com>
2023-05-11 17:41:58 +0600

    for_pull

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  //For git pull
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
